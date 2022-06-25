7943f903cc61fa06b781f6f69698578b498acb21
bahadirsonmez <sonmezbahad@gmail.com>
2022-06-25 15:11:21 +0300

    Create project

---
+++ network-layer/network_layerApp.swift
@@ -0,0 +1,17 @@
+//
+//  network_layerApp.swift
+//  network-layer
+//
+//  Created by Bahadır Kişisel on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct network_layerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
