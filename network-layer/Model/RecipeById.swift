007b633a9407532c142918c6c36698f43b453004
bahadirsonmez <sonmezbahad@gmail.com>
2022-06-28 19:28:02 +0300

    Create recipe by id model

--- network-layer/Model/RecipeById.swift
+++ network-layer/Model/RecipeById.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
-struct RecipeById {
-    
+typealias RecipesDictionary = [String: RecipeById]
+
+struct RecipeById: Codable {
+    let name: String
+    let price: Int
+    let ingredients: [String]
+    let hasCheese, hasSalt: Bool
 }
