c3af505c2be0046233292c4d0fcc823b83dafbe1
bahadirsonmez <sonmezbahad@gmail.com>
2022-06-29 09:26:56 +0300

    Add dummy view

--- network-layer/ContentView.swift
+++ network-layer/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert: Bool = false
+    @State private var alertText: String = ""
+    @State private var data: [Recipe] = []
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            Button {
+                showAlert.toggle()
+            } label: {
+                Text("Hit me!")
+            }
+            .alert(alertText, isPresented: $showAlert, actions: { })
+        }.onAppear {
+            let request = GetAllRecipesRequest()
+            RecipesService().getAllRecipes(request: request) { result in
+                switch result {
+                case .success(let data):
+                    self.data = data
+                case .failure(let error):
+                    alertText = error.localizedDescription
+                    showAlert.toggle()
+                }
+            }
+        }
     }
 }
 
